2e3cf5dcfe8db7787a8665cecaa5de79353551ce
Ayegba Joshua <ayegbajoshua@yahoo.com>
2022-02-28 11:45:57 +0100

    First initialisation

---
+++ hcjPracticeJromtechWebsiteRedesign/main.js
@@ -0,0 +1,16 @@
+let p =  new Promise(let p = new 
+
+    const Promise = ((resolve, reject) => {
+        let a = 1 + 1
+        if (a == 2) {
+            resolve("Success")
+        } else {
+            reject ("Failed!")
+        }
+    
+    )})
+
+
+
+
+
